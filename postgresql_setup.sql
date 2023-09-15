67b34289736d541e955ed54043ac642f432bc4d5
mncy-earth <80756702+mncy-earth@users.noreply.github.com>
2023-09-15 15:36:28 +0700

    Update postgresql_setup.sql

--- postgresql_setup.sql
+++ postgresql_setup.sql
@@ -1,14 +1,4 @@
--- DROP SCHEMA IF EXISTS public;
--- CREATE SCHEMA public;
-
--- ALTER USER postgres_user SET search_path = public;
-
-----------------------------------------------------------------------------------------------------
-
--- DROP ROLE IF EXISTS dwh_user;
 CREATE ROLE dwh_user SUPERUSER LOGIN PASSWORD 'dwh_pass';
-
--- DROP SCHEMA IF EXISTS dwh;
 CREATE SCHEMA dwh;
 GRANT ALL ON ALL TABLES IN SCHEMA dwh TO dwh_user;
 ALTER USER dwh_user SET search_path = dwh;
@@ -23,9 +13,6 @@ CREATE TABLE dwh.data_sample (
 
 ----------------------------------------------------------------------------------------------------
 
--- DROP DATABASE IF EXISTS airflow_db;
 CREATE DATABASE airflow_db;
-
--- DROP ROLE IF EXISTS airflow_user;
 CREATE USER airflow_user WITH PASSWORD 'airflow_pass';
 GRANT ALL PRIVILEGES ON DATABASE airflow_db TO airflow_user;
